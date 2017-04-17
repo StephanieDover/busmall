ffa8ecd1eb4a8eca7c2be4171e547575bd24fcaf
Stephanie Dover <doverstephaniem@gmail.com>
2017-04-17 16:32:16 -0700

    created for loop to loop through productsArray length and generate 3 random numbers. used random numbers to write filePath as image src in img element. displayed in the dom

--- bus.js
+++ bus.js
@@ -1,12 +1,11 @@
 'use strict';
 
 function Product(name, filePath) {
-  this.name ='name';
-  this.filePath= 'filePath';
+  this.name = name ;
+  this.filePath= filePath;
   this.shows = 0;
   this.clicks = 0;
 }
-
 
 var bag = new Product('Bag','img/bag.jpg');
 var banana = new Product('Banana Cutter', 'img/banana.jpg');
@@ -27,6 +26,38 @@ var tauntaun = new Product ('tauntaun', 'img/tauntaun.jpg');
 var unicorn = new Product ('unicorn', 'img/unicorn.jpg');
 var usb = new Product ('USB', 'img/usb.gif');
 var watering = new Product ('Watering Can', 'img/water-can.jpg');
-var wine = new Product ('Wine Glass', 'wine-glass.jpg');
+var wine = new Product ('Wine Glass', 'img/wine-glass.jpg');
 
 var productsArray = [bag, banana, bathroom, boot, breakfast, bubblegum, chair, cthulhu, duck, dragon, pen, sweepPet, scissors, shark, sweepBaby, tauntaun, unicorn, usb, watering, wine];
+
+var displayProduct = function(){
+  for ( var productLoop = 0; productLoop < 1; productLoop++) {
+    var randomNum = Math.floor(Math.random()* productsArray.length);
+
+    var el = document.getElementById('img-1');
+    var image = document.createElement('img');
+    image.src = productsArray[randomNum].filePath;
+    el.appendChild(image);
+
+    do {
+      var randomNumTwo = Math.floor(Math.random()* productsArray.length);
+    } while ( randomNumTwo === randomNum);
+    el = document.getElementById('img-2');
+    image = document.createElement('img');
+    image.src = productsArray[randomNumTwo].filePath;
+    el.appendChild(image);
+
+    do {
+      var randomNumThree = Math.floor(Math.random()* productsArray.length);
+    } while ( randomNumTwo === randomNumThree || randomNumThree === randomNum);
+    el = document.getElementById('img-3');
+    image = document.createElement('img');
+    image.src = productsArray[randomNumThree].filePath;
+    el.appendChild(image);
+
+
+    console.log(productsArray[productLoop].filePath);
+
+  }
+};
+displayProduct();
